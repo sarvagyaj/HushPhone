2e6231a52d840a265ff6fee1891dc9e3831da2af
sarvagyaj <sarvagyaj@gmail.com>
2014-02-27 09:24:28 -0800

    Google map displays

---
+++ HushPhone/src/com/hyp/maps/MapOverlay.java
@@ -0,0 +1,22 @@
+package com.hyp.maps;
+
+import android.view.MotionEvent;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapController;
+import com.google.android.maps.Overlay;
+
+/*public class MapOverlay extends Overlay  {
+	//GoogleMap map;
+	MapController mc;
+	//GeoPoint p;
+	
+	public boolean onTouchEvent(MotionEvent event, GoogleMap map) {
+		//user lifts finger
+		if(event.getAction()==1) {
+			//GeoPoint p =map.getProjection().fromScreenLocation(event.getX())
+		}
+	}
+}
+*/
